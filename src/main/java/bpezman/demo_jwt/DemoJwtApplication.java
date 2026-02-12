25073c503c8f8e0feb60577360f386a86bb2830a
brunopez <bpezman@fi.uba.ar>
2026-02-12 23:31:18 +0000

    modif pom.xml, Updated Spring Boot Version to 4.0.2

--- src/main/java/bpezman/demo_jwt/DemoJwtApplication.java
+++ src/main/java/bpezman/demo_jwt/DemoJwtApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoJwtApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(DemoJwtApplication.class, args);
   }
